d757ba31ecc9c685a4b97d5228b9f8d23c2e2ef1
renpengcheng4612 <871340705@qq.com>
2021-09-27 23:46:29 +0800

    springBoot Demo git init

---
+++ src/test/java/cn/chinaclear/org/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chinaclear.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
