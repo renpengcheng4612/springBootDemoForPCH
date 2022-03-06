eda171d66cb52f121eed02c47c9aed3db4736f82
焱宣 <871340705@qq.com>
2022-03-06 18:19:07 +0800

    feat:  新增mybatis-plus-test 源码

---
+++ Mybatis-plus-Test/src/main/java/com/pc/sz/model/po/User.java
@@ -0,0 +1,16 @@
+package com.pc.sz.model.po;
+
+import lombok.Data;
+
+/**
+ * @author 焱宣
+ * @Description
+ * @create 2022-03-06 13:16
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
